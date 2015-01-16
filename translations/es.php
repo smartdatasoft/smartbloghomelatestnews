57d5bbf7ae31e18c9be178ef2ec5c0f4413ef360
Rodolfo Castellanos <rodolfojcj at yahoo dot com>
2015-01-16 15:39:09 -0430

    Added spanish translation

---
+++ translations/es.php
@@ -0,0 +1,13 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartbloghomelatestnews_660a5e003bce0177d7aa7b39c1c0df5c'] = 'SmartBlog - Novedades al Inicio';
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartbloghomelatestnews_a14895030612094493056c982a7089c9'] = 'El módulo de blog más poderoso para Prestashop - por smartdatasoft';
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartbloghomelatestnews_fa214007826415a21a8456e3e09f999d'] = '¿Está seguro de que quiere borrar los detalles?';
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartbloghomelatestnews_21ee0d457c804ed84627ec8345f3c357'] = 'Configuración actualizada exitosamente.';
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartbloghomelatestnews_c54f9f209ed8fb4683e723daa4955377'] = 'Conguración general';
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartbloghomelatestnews_ea2766a70e61de99505624aa16055d4f'] = 'Número de entradas a mostrar en novedades';
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartbloghomelatestnews_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartblog_latest_news_099afb04ddf259fff6a50c50d7eae060'] = 'Novedades más recientes';
+$_MODULE['<{smartbloghomelatestnews}prestashop>smartblog_latest_news_decbe415d8accca7c2c16d48a79ee934'] = 'Leer más';
